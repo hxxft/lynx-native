2725b78226655f4f3c1dad739f7eef9b01680afd
yxp <yxpflyman@gmail.com>
2017-11-21 14:33:22 +0800

    [Feat] Add JS Example

---
+++ Example/src/CoordinatorPage.vue
@@ -0,0 +1,256 @@
+<template>
+  <view
+    class="container"
+    id="app">
+
+    <scrollview
+      ref="vScrollView"
+      class="vertical-scrollview"
+      coordinator-tag="0"
+      coordinator-type="scroll"
+      coordinator-affinity="scroll">
+      <scrollview
+        class="scrollview"
+        page-enable="true"
+        coordinator-tag="1"
+        coordinator-type="scroll"
+        coordinator-affinity="scroll">
+        <view
+          v-for="item in contentList"
+          class="scrollview-item">
+          <view class="item-wrap">
+            <label class="item-title">{{item.title}}</label>
+          </view>
+          <view class="item-wrap">
+            <img class="item-img" :src="item.imgUrl"></img>
+          </view>
+          <template v-for="i in 7">
+            <view class="line"></view>
+            <view class="item-wrap">
+              <img class="item-img-small" :src="item.tinyImgUrl"></img>
+              <label class="item-content">{{item.content}}</label>
+            </view>
+          </template>
+        </view>
+      </scrollview>
+    </scrollview>
+
+    <view
+      class="anim-bg"
+      coordinator-affinity="scroll"
+      coordinator-command="scroll:onScrollEventForBg">
+      <template v-for="(anim, index) in animList">
+        <view
+          class="anim-card"
+          @click="onClick(index)"
+          :style="{
+            marginTop: index * 310 + ''
+          }"
+          coordinator-affinity="scroll"
+          :coordinator-tag="index"
+          coordinator-command="scroll:onScrollEventForCard">
+          <img
+            class="anim-card-img"
+            :src="anim.imgUrl"
+            coordinator-affinity="scroll"
+            :coordinator-tag="index"
+            coordinator-command="scroll:onScrollEventForImg"></img>
+        </view>
+        <label
+          class="anim-label"
+          :style="{
+            marginTop: index * 310 + ''
+          }"
+          coordinator-affinity="scroll"
+          :coordinator-tag="index"
+          coordinator-command="scroll:onScrollEventForLabel">{{anim.title}}</label>
+        <view
+          class="anim-line"
+          :style="{
+            marginTop: index * 310 + ''
+          }"
+          coordinator-affinity="scroll"
+          :coordinator-tag="index"
+          coordinator-command="scroll:onScrollEventForLine"></view>
+      </template>
+    </view>
+
+    <img
+      class="back-btn"
+      src="icon_back.png"></img>
+  </view>
+
+</template>
+
+<script>
+import animationCommand from './coordinator_anim_bar.lepus'
+export default {
+  components: {
+  },
+  data() {
+    return {
+      contentList: [
+        {
+          title: "Lynx",
+          imgUrl: "pic_lynx.png",
+          tinyImgUrl: "icon_lynx_sit.png",
+          content: "Interactive animation support in Lynx"
+        },
+        {
+          title: "Cat",
+          imgUrl: "pic_cat.png",
+          tinyImgUrl: "icon_cat.png",
+          content: "Interactive animation support in Lynx"
+        },
+        {
+          title: "Tiger",
+          imgUrl: "pic_tiger.png",
+          tinyImgUrl: "icon_tiger.png",
+          content: "Interactive animation support in Lynx"
+        },
+        {
+          title: "Lion",
+          imgUrl: "pic_lion.png",
+          tinyImgUrl: "icon_lion.png",
+          content: "Interactive animation support in Lynx"
+        }
+      ],
+      animList: [
+        {
+          title: "Lynx",
+          imgUrl: "pic_lynx.png",
+        },
+        {
+          title: "Cat",
+          imgUrl: "pic_cat.png",
+        },
+        {
+          title: "Tiger",
+          imgUrl: "pic_tiger.png",
+        },
+        {
+          title: "Lion",
+          imgUrl: "pic_lion.png",
+        }
+
+      ]
+    }
+  },
+  mounted() {
+    LynxCoordinatorRegister.registeAction("scroll", "scroll", animationCommand)
+  },
+  methods: {
+    onClick(index) {
+      console.log("click")
+      this.$refs.vScrollView.scrollTop = 1240 - 120
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  flex-direction: row;
+}
+.vertical-scrollview {
+  flex-direction: column;
+}
+.scrollview {
+  padding-top: 1240;
+  flex-direction: row;
+  width: 750;
+}
+.scrollview-item {
+  flex-direction: column;
+  width: 750;
+}
+.shadow {
+  position: absolute;
+  left: 0;
+  top: 120;
+  width: 750;
+  height: 16;
+  object-fit: fill;
+  opacity: 0;
+}
+
+.item-wrap {
+  flex-direction: row;
+  padding-top: 40;
+  padding-bottom: 40;
+  padding-left: 30;
+  padding-right: 30;
+  align-items: center;
+}
+.item-title {
+  color: black;
+  font-size: 50;
+  font-weight: bold;
+}
+.item-img {
+  width: 700;
+  height: 380;
+}
+.item-img-small {
+  width: 50;
+  height: 50;
+}
+.item-content {
+  color: black;
+  font-size: 35;
+  margin-left: 20;
+}
+.line {
+  height: 2;
+  align-self: stretch;
+  background-color: #d0d0d0;
+}
+.back-btn {
+  position: absolute;
+  height: 50;
+  width: 50;
+  top: 30;
+  left: 30;
+  object-fit: fill;
+}
+.anim-bg {
+  position: absolute;
+  width: 750;
+  height: 1240;
+  background-color: #66ccff;
+}
+.anim-card {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 300;
+  width: 500;
+}
+.anim-card-img {
+  height: 400;
+  width: 600;
+  margin-top: -50;
+  margin-left: -50;
+  z-index: 0;
+}
+.anim-label {
+  position: absolute;
+  color: white;
+  font-size: 60;
+  line-height: 60;
+  left: 60;
+  top: 120;
+  font-weight: bold;
+  z-index: 100;
+}
+.anim-line {
+  position: absolute;
+  background-color: white;
+  border-radius: 4;
+  height: 8;
+  width: 80;
+  right: 40;
+  top: 250;
+  z-index: 100;
+}
+</style>
---
+++ Example/src/GalleryPage.vue
@@ -0,0 +1,199 @@
+<template>
+  <view
+    class="container"
+    id="app">
+    <listview
+      class="scrollview">
+      <view class="placeholder">
+        <view class="img-wrap">
+          <img
+            class="img-show"
+            :src="firstBgImg"
+            coordinator-affinity="touch"
+            coordinator-tag="201"
+            coordinator-command="touch:onTouchEventForBg"
+            ></img>
+          <img
+            class="img-show"
+            :src="secondBgImg"
+            coordinator-affinity="touch"
+            coordinator-tag="202"
+            coordinator-command="touch:onTouchEventForBg"
+            ></img>
+          <img
+            class="img-show"
+            :src="thirdBgImg"
+            coordinator-affinity="touch"
+            coordinator-tag="203"
+            coordinator-command="touch:onTouchEventForBg"
+            ></img>
+          <img
+            class="img-show"
+            src="pic_mask.png"></img>
+        </view>
+        <view
+          class="scroll-wrap"
+          coordinator-affinity="touch"
+          coordinator-tag="100"
+          coordinator-type="touch"
+          @onpagechanged="onPageChanged"
+          coordinator-command="touch:onTouchEventForWrap">
+          <img
+            v-for="(image, index) in images"
+            class="scroll-item-image"
+            coordinator-affinity="touch"
+            :coordinator-tag="index"
+            coordinator-command="touch:onTouchEventForImg"
+            :src="image"></img>
+        </view>
+      </view>
+      <view class="intro-wrap">
+        <label class="intro-name">{{animals[curPage].name}}</label>
+        <label class="intro-content">{{animals[curPage].intro}}</label>
+        <img class="intro-img" :src="animals[curPage].pic"></img>
+      </view>
+    </listview>
+
+  </view>
+
+</template>
+
+<script>
+import animationAction from './coordinator_3d_scroll.lepus'
+export default {
+  components: {
+  },
+  data() {
+    return {
+      firstBgImg: 'pic_lynx_vertical.png',
+      secondBgImg: 'pic_cat_vertical.png',
+      thirdBgImg: 'pic_tiger_vertical.png',
+      curPage: 0,
+      images: [
+        'pic_lynx_vertical.png',
+        'pic_cat_vertical.png',
+        'pic_tiger_vertical.png',
+        'pic_lion_vertical.png'
+      ],
+      bgImages: [
+        'pic_lynx_vertical.png',
+        'pic_cat_vertical.png',
+        'pic_tiger_vertical.png'
+      ],
+      animals: [
+        {
+          name: 'Lynx',
+          intro: 'Lynx have a short tail, characteristic tufts of black hair on the tips of their ears, large, padded paws for walking on snow and long whiskers on the face. Under their neck, they have a ruff which has black bars resembling a bow tie although this is often not visible.',
+          pic: 'pic_lynx.png'
+        },
+        {
+          name: 'Cat',
+          intro: 'The domestic cat (Felis silvestris catus or Felis catus) is a small, typically furry, carnivorous mammal. They are often called house cats when kept as indoor pets or simply cats when there is no need to distinguish them from other felids and felines. Cats are often valued by humans for companionship and for their ability to hunt vermin. There are more than 70 cat breeds, though different associations proclaim different numbers according to their standards.',
+          pic: 'pic_cat.png'
+        },
+        {
+          name: 'Tiger',
+          intro: 'The tiger (Panthera tigris) is the largest cat species, most recognizable for their pattern of dark vertical stripes on reddish-orange fur with a lighter underside. The species is classified in the genus Panthera with the lion, leopard, jaguar, and snow leopard. Tigers are apex predators, primarily preying on ungulates such as deer and bovids. They are territorial and generally solitary but social animals, often requiring large contiguous areas of habitat that support their prey requirements. This, coupled with the fact that they are indigenous to some of the more densely populated places on Earth, has caused significant conflicts with humans.',
+          pic: 'pic_tiger.png'
+        },
+        {
+          name: 'Lion',
+          intro: 'The lion (Panthera leo) is one of the big cats in the Felidae family and a member of genus Panthera. It has been listed as Vulnerable on the IUCN Red List since 1996, as populations in African range countries declined by about 43% since the early 1990s. Lion populations are untenable outside designated protected areas. Although the cause of the decline is not fully understood, habitat loss and conflicts with humans are the greatest causes of concern. The West African lion population is listed as Critically Endangered since 2016. The only lion population in Asia survives in and around Indias Gir Forest National Park and is listed as Endangered since 1986.',
+          pic: 'pic_lion.png'
+        }
+      ]
+    }
+  },
+  mounted() {
+    LynxCoordinatorRegister.registeAction("touch", "touch", animationAction)
+  },
+  methods: {
+    onPageChanged(e) {
+      var curIndex = e.detail
+      this.curPage = curIndex
+      var curShowBgIndex = curIndex % 3
+      if (curShowBgIndex == 0) { // FirstBgImg show
+        this.firstBgImg = this.images[curIndex]
+        if (curIndex + 1 <= this.images.length - 1) {
+          this.secondBgImg = this.images[curIndex + 1]
+        }
+        if (curIndex - 1 >= 0) {
+          this.thirdBgImg = this.images[curIndex - 1]
+        }
+      } else if (curShowBgIndex == 1) { // SecondBgImg show
+        this.firstBgImg = this.images[curIndex - 1]
+        this.secondBgImg = this.images[curIndex]
+        if (curIndex + 1 <= this.images.length - 1) {
+          this.thirdBgImg = this.images[curIndex + 1]
+        }
+      } else if (curShowBgIndex == 2) { // ThirdBgImg show
+        this.secondBgImg = this.images[curIndex - 1]
+        this.thirdBgImg = this.images[curIndex]
+        if (curIndex + 1 <= this.images.length - 1) {
+          this.firstBgImg = this.images[curIndex + 1]
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  flex-direction: row;
+}
+.scrollview {
+  flex-direction: column;
+  width: 750;
+}
+.placeholder {
+  height: 900;
+  width: 750;
+}
+.img-wrap {
+  position: absolute;
+  width: 750;
+  height: 750;
+}
+.img-show {
+  position: absolute;
+  width: 750;
+  height: 750;
+}
+.scroll-wrap {
+  position: absolute;
+  flex-direction: row;
+  top: 200;
+  width: 750;
+  height: 700;
+}
+.scroll-item-image {
+  border-radius: 20;
+  width: 450;
+  height: 700;
+  margin-left: 40;
+}
+
+.intro-wrap {
+  margin-top: 50;
+  margin-bottom: 50;
+  width: 600;
+  align-self: center;
+  flex-direction: column;
+}
+.intro-name {
+  font-size: 70;
+  font-weight: bold;
+  width: 200;
+}
+.intro-content {
+  margin-top: 40;
+  font-size: 40;
+  width: 600;
+}
+.intro-img {
+  margin-top: 40;
+  width: 600;
+  height: 300;
+}
+</style>
