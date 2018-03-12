ea7c017ca1b59aebe10b751493ccd5a7a8156378
yxp <yxpflyman@gmail.com>
2018-03-12 18:41:22 +0800

    [Fix] Label layout bug

--- lynx/render/label.cpp
+++ lynx/render/label.cpp
@@ -51,51 +51,49 @@ base::Size Label::OnMeasure(int width_descriptor, int height_descriptor) {
     int height_wanted = (int) style_.height_;
     int width_mode = base::Size::Descriptor::GetMode(width_descriptor);
     int height_mode = base::Size::Descriptor::GetMode(height_descriptor);
-    width_descriptor = base::Size::Descriptor::GetSize(width_descriptor);
-    height_descriptor = base::Size::Descriptor::GetSize(height_descriptor);
+    int width = base::Size::Descriptor::GetSize(width_descriptor);
+    int height = base::Size::Descriptor::GetSize(height_descriptor);
 
-    width_descriptor = CSS_IS_UNDEFINED(width_wanted)
-            && !CSS_IS_UNDEFINED(width_descriptor)
+    width = CSS_IS_UNDEFINED(width_wanted)
+            && !CSS_IS_UNDEFINED(width)
             && (width_mode == base::Size::Descriptor::EXACTLY
                 || width_mode == base::Size::Descriptor::AT_MOST)?
-            (int) style_.ClampExactWidth(width_descriptor) :
+            (int) style_.ClampExactWidth(width) :
             (int) style_.ClampWidth();
-    height_descriptor = CSS_IS_UNDEFINED(height_wanted)
-             && !CSS_IS_UNDEFINED(height_descriptor)
+    height = CSS_IS_UNDEFINED(height_wanted)
+             && !CSS_IS_UNDEFINED(height)
              && (height_mode == base::Size::Descriptor::EXACTLY
                  || height_mode == base::Size::Descriptor::AT_MOST)?
-             (int) style_.ClampExactHeight(height_descriptor) :
+             (int) style_.ClampExactHeight(height) :
              (int) style_.ClampHeight();
 
-    width_descriptor -= style_.padding_left_ + style_.padding_right_
-             + style_.border_width_ * 2;
-    height_descriptor -= style_.padding_top_ + style_.padding_bottom_
-              + style_.border_width_ * 2;
+    if (!CSS_IS_UNDEFINED(width)) {
+        width -= style_.padding_left_ + style_.padding_right_
+                            + style_.border_width_ * 2;
+    }
+    if (!CSS_IS_UNDEFINED(height)) {
+        height -= style_.padding_top_ + style_.padding_bottom_
+                  + style_.border_width_ * 2;
+    }
 
     base::Size size =
             LabelMeasurer::MeasureLabelSizeAndSetTextLayout(
-                    this,
-                    base::Size(width_descriptor, height_descriptor),
+                    this, base::Size(width, height),
                     text_node_ == NULL ? GetText() : text_node_->GetText());
-
 
     size.width_ += style_.padding_left_ + style_.padding_right_
                    + style_.border_width_ * 2;
     size.height_ += style_.padding_top_ + style_.padding_bottom_
                     + style_.border_width_ * 2;
 
-    size.width_ = (int) style_.ClampWidth(size.width_);
-    size.height_ = (int) style_.ClampHeight(size.height_);
-
-
-    size.width_ = !CSS_IS_UNDEFINED(width_descriptor)
+    size.width_ = !CSS_IS_UNDEFINED(width)
                           && width_mode == base::Size::Descriptor::EXACTLY ?
-                  (int) style_.ClampExactWidth(base::Size::Descriptor::GetSize(width_descriptor)) :
+                  (int) style_.ClampExactWidth(width) :
                   (int) style_.ClampWidth(size.width_);
-    size.height_ = !CSS_IS_UNDEFINED(height_descriptor)
+    size.height_ = !CSS_IS_UNDEFINED(height)
                            && height_mode == base::Size::Descriptor::EXACTLY ?
-                   (int) style_.ClampExactHeight(base::Size::Descriptor::GetSize(height_descriptor)) :
-                   (int) style_.ClampHeight(size.height_);
+                   (int) style_.ClampExactHeight(height) :
+                   (int) style_.ClampExactHeight(size.height_);
 
     measured_size_ = size;
     return size;
