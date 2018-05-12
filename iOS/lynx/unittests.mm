7ed0f9bd2c32317451fe87649e859d2a5a759bfb
dli <leedongjx@gmail.com>
2018-05-12 12:34:58 +0800

    update unittest for iOS

---
+++ iOS/lynx/unittests.mm
@@ -0,0 +1,10 @@
+// Copyright 2017 The Lynx Authors. All rights reserved.
+
+#import <UIKit/UIKit.h>
+#include "app_delegate.h"
+#include "gtest/gtest.h"
+
+int main(int argc, char * argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
